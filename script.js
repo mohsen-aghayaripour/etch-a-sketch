3ed719009db98ec69272376b487514d53de6dcbc
mohsen-aghayaripour <mohsen.aghayaripour@gmail.com>
2021-05-07 19:58:46 +0200

    add mouse over event listener

--- script.js
+++ script.js
@@ -14,5 +14,12 @@ container.style.gridTemplateColumns = "repeat(16,1fr)";
 for (let i = 0; i < 16*16; i++){
     let myDiv = document.createElement("div");
     myDiv.classList.add("grid");
+    myDiv.addEventListener('mouseover', mouseMove);
     container.appendChild(myDiv);
+}
+function mouseMove(e){
+    let randr = Math.floor(Math.random()*256);
+    let randc = Math.floor(Math.random()*256);
+    let randb = Math.floor(Math.random()*256);
+    e.target.style.background = `rgb(${randr}, ${randc}, ${randb})` ;
 }
