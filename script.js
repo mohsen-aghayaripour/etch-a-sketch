be3a1a834153795f8028ad1befdf8fe28208a634
mohsen-aghayaripour <mohsen.aghayaripour@gmail.com>
2021-05-13 20:12:07 +0200

    add button js functionality

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 
 const container = document.querySelector('#container');
-const btn = document.querySelector('#btn');
-// container.style.display = 'grid';
-// container.style.maxWidth = '500px';
-// container.style.minHeight = '500px';
+const btn = document.getElementById('btn').addEventListener("click", myFunction);
+window.addEventListener('load', defultGrid())
+function defultGrid(){
+    makeRows(16);
+    gridTempColumns(16);
+}
+function myFunction(){
+    let maxGrid = prompt("enter your grid numbers:");
+    if(maxGrid !== null){
+        maxGrid = parseInt(maxGrid);
+        if(maxGrid > 65 || maxGrid < 1 || Number.isNaN(maxGrid)){
+        alert('Enter a number between 1 and 64!');
+        myFunction();
+        }
+        else{
+            reset();
+            makeRows(maxGrid);
+            gridTempColumns(maxGrid)
+        }
+    }
+}
+function gridTempColumns(size) {container.style.gridTemplateColumns = `repeat(${size},1fr)`;}
 
-// display: grid;
-// max-width: 500px;
-// min-height: 500px;
-// margin: 2rem auto;
-// border: 1px solid var(--main-dark);
-
-container.style.gridTemplateColumns = "repeat(16,1fr)";
-for (let i = 0; i < 16*16; i++){
-    let myDiv = document.createElement("div");
-    myDiv.classList.add("grid");
-    myDiv.addEventListener('mouseover', mouseMove);
-    container.appendChild(myDiv);
+function makeRows(size){
+    for (let i = 0; i < size * size; i++){
+        let myDiv = document.createElement("div");
+        myDiv.classList.add("grid");
+        myDiv.addEventListener('mouseover', mouseMove);
+        container.appendChild(myDiv);
+    }
 }
 function mouseMove(e){
     let randr = Math.floor(Math.random()*256);
     let randc = Math.floor(Math.random()*256);
     let randb = Math.floor(Math.random()*256);
     e.target.style.background = `rgb(${randr}, ${randc}, ${randb})` ;
+}
+function reset(){
+    document.querySelectorAll('.grid').forEach((e) => e.parentNode.removeChild(e));
 }
